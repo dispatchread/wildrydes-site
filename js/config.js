4bc381a48462db74a72767cbcb8e650a92c4f5fe
Dispatch Read <dispatchread@gmail.com>
2022-05-24 22:18:18 +0000

    ddd

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_JVP6JfpTh', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '74up4rmvcf44ehsomfuc2939bl', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-west-2' // e.g. us-east-2
+        userPoolId: 'us-east-1_OIrlMrw5o', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3ifsukeefl9h0no1stuj2m5071', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-	    invokeUrl: 'https://o56mapjtnelztyjpvxdbaj3yey0owtfn.lambda-url.us-east-1.on.aws/' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
